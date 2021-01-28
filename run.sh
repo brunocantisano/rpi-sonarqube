b2d69a402e1d012d44cfa202264d9850b5d8fed5
Bruno Cardoso Cantisano <bruno.cantisano@gmail.com>
2021-01-28 19:06:11 -0300

    adicionando shell para execução

--- run.sh
+++ run.sh
@@ -2,12 +2,9 @@
 #
 if [ ! "$1" = "" ]; then
     echo -e "\ncreating container $1\n\n"
-    docker run -d -p 9408:9000 \
-               --env DB_USER=sonar \
-               --env DB_PASS=xaexohquaetiesoo \
-               --env DB_NAME=sonar \
-               --env DB_TYPE=POSTGRES \
-               --name $1 paperinik/rpi-sonarqube
+    docker run -u 0 -d -p 9408:9000 \
+               -v `pwd`/sonar-scanner/plugins:/sonarqube-$(DOCKER_IMAGE_VERSION)/extensions/plugins \
+               --name $1 paperinik/rpi-sonarqube:latest
 else
     echo -e "\nUSAGE: run.sh your-container-name \n\n"
 fi
