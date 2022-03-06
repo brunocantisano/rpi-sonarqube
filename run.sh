4521bf996a39a7f8250df9189d3e4aabfe73770d
Bruno Cardoso Cantisano <bruno.cantisano@gmail.com>
2022-03-06 16:11:34 -0300

    adding parameters to execute using postgres

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 #
 if [ ! "$1" = "" ] | [ ! "$2" = "" ]; then
     echo -e "\ncreating container $1, sonar version: $2 \n\n"
-    docker run --rm -u sonarqube -d --name $1 -p 9408:9000 \
+    docker run -u sonarqube -d --name $1 -p 9408:9000 \
                -v ~/projetos/dados/sonar-scanner/plugins:/sonarqube-$2/extensions/plugins \
+               -e SONARQUBE_JDBC_USERNAME="USER" \
+               -e SONARQUBE_JDBC_PASSWORD="PASSWORD" \
+               -e SONARQUBE_JDBC_URL="jdbc:postgresql://HOST/DATABASE" \
                paperinik/rpi-sonarqube:latest
-
 else
     echo -e "\nUSAGE: run.sh your-container-name sonar-version \n\n"
 fi
